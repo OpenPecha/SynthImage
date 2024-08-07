1260d3cadefd64c37cf90ba35ca061e5b6f10825
jim-gyas <jackandthebean1234@gmail.com>
2024-08-07 15:41:22 +0530

    added documentation and integration testing

--- src/SynthImage/line_extraction.py
+++ src/SynthImage/line_extraction.py
@@ -4,26 +4,41 @@ from PIL import Image, ImageDraw, ImageFont
 
 class ExtractLines:
     def __init__(self, aug_img, page_text, rotation_angle, font_size, font_path):
+        """Initialize the ExtractLines object.
+
+        Args:
+            aug_img (PIL.Image.Image): The augmented image from which to extract lines.
+            page_text (str):The text content of the page.
+            rotation_angle (int): The angle to rotate the image for line extraction.
+            font_size (int): The size of the font used in the image.
+            font_path (str): The path to the font file used in the image.
+        """
         self.aug_img = aug_img
         self.page_text = page_text
         self.rotation_angle = rotation_angle
         self.font_size = font_size
         self.font_path = font_path
 
-    def extract_lines(self):
-        if self.rotation_angle != 0:
-            self.aug_img = self.aug_img.rotate(
-                -self.rotation_angle, expand=True, fillcolor=(255, 255, 255)
-            )
+    def get_blank_img(self):
+        """Create a blank white image with the same size as the augmented image.
 
-        lines = self.page_text.split("\n")
-        font = ImageFont.truetype(self.font_path, self.font_size, encoding="utf-16")
-        line_images = []
+        Returns:
+            PIL.Image.Image: A blank white image.
+        """
+        return Image.new("RGB", self.aug_img.size, (255, 255, 255))
 
-        # Create a blank image to draw text
-        blank_img = Image.new("RGB", self.aug_img.size, (255, 255, 255))
+    def get_max_width(self, lines, blank_img, font):
+        """Determine the maximum width of the lines of text and their bounding boxes.
 
-        # Step 1: Determine the maximum width
+        Args:
+            lines (list of str): The lines of text.
+            blank_img (_PIL.Image.Image):A blank image used for calculating bounding boxes.
+            font (PIL.ImageFont.FreeTypeFont): The font used for the text.
+
+        Returns:
+            tuple: The maximum width of the lines and a list of tuples, each containing a
+            line of text and its bounding box.
+        """
         max_width = 0
         line_bboxes = []
         # Find the topmost position of the text after rotation
@@ -32,6 +47,7 @@ class ExtractLines:
             np.min(non_white_pixels[0]).item() if non_white_pixels[0].size > 0 else 0,
             30,
         )  # Start position for the first line
+
         for line in lines:
             # Draw the line on the blank image to calculate the bounding box
             temp_img = blank_img.copy()
@@ -45,12 +61,24 @@ class ExtractLines:
 
             # Move to the next line position
             y += line_bbox[3] - line_bbox[1]
+        return max_width, line_bboxes
 
-        # Step 2: Extract lines with the maximum width
+    def get_line_images(self, max_width, line_bboxes):
+        """Extract images of each line of text based on their bounding boxes.
+
+        Args:
+            max_width (int): The maximum width of the lines.
+            line_bboxes (list of tuples): A list of tuples, each containing a line of text and its bounding box.
+        Returns:
+            list of PIL.Image.Image:  A list of images, each containing a line of text.
+        """
+        line_images = []
+        non_white_pixels = np.where(np.array(self.aug_img) != 255)
         y = max(
             np.min(non_white_pixels[0]).item() if non_white_pixels[0].size > 0 else 0,
             30,
-        )  # Reset start position for extraction
+        )  # Start position for the first line
+
         for line, bbox in line_bboxes:
             # Adjust bounding box width to max_width
             x1, y1, x2, y2 = bbox
@@ -70,6 +98,29 @@ class ExtractLines:
 
             # Move to the next line position
             y += bbox[3] - bbox[1]
+        return line_images
+
+    def extract_lines(self):
+        """Extract individual line images from the augmented image.
+
+        Returns:
+           list of PIL.Image.Image:  A list of images, each containing a line of text.
+        """
+        if self.rotation_angle != 0:
+            self.aug_img = self.aug_img.rotate(
+                -self.rotation_angle, expand=True, fillcolor=(255, 255, 255)
+            )
+
+        lines = self.page_text.split("\n")
+        font = ImageFont.truetype(self.font_path, self.font_size, encoding="utf-16")
+
+        # Create a blank image to draw text
+        blank_img = self.get_blank_img()
+        # Determine the maximum width
+        max_width, line_bboxes = self.get_max_width(lines, blank_img, font)
+
+        # Extract lines with the maximum width
+        line_images = self.get_line_images(max_width, line_bboxes)
 
         if self.rotation_angle != 0:
             for i in range(len(line_images)):
--- tests/line_extraction/test_extraction.py
+++ tests/line_extraction/test_extraction.py
@@ -1,7 +1,7 @@
 import tempfile
 from pathlib import Path
 
-from PIL import Image
+from PIL import Image, ImageFont
 
 from SynthImage.line_extraction import ExtractLines
 
@@ -10,6 +10,7 @@ original_img_path = Path(
 )
 font_path = "./tests/font/monlam_uni_ochan1.ttf"
 original_img_obj = Image.open(original_img_path)
+
 text = """  ༄༅༅། །རྒྱ་གར་སྐད་དུ། བི་ན་ཡ་བསྟུ། བོད་སྐད་དུ། འདུལ་བ་གཞི། བམ་པོ་དང་པོ། དཀོན་མཆོག་གསུམ་ལ་ཕྱག་འཚལ་ལོ། །གང་གིས་འཆིང་
             རྣམས་ཡང་དག་རབ་བཅད་ཅིང་། །མུ་སྟེགས་ཚོགས་རྣམས་ཐམས་ཅད་རབ་བཅོམ་སྟེ། །སྡེ་དང་བཅས་པའི་བདུད་རྣམས་ངེས་བཅོམ་ནས། །བྱང་ཆུབ་འདི་བརྙེས་དེ་ལ་
             ཕྱག་འཚལ་ལོ། །ཁྱིམ་དོན་ཆེ་ཆུང་སྤངས་ཏེ་དང་པོར་རབ་འབྱུང་དཀའ། །རབ་བྱུང་ཐོབ་ནས་ཡུལ་སྤྱད་དག་གིས་དགའ་ཐོབ་དཀའ། །མངོན་དགའ་ཇི་བཞིན་དོན་བསྐྱེད་ཡང་
@@ -20,6 +21,12 @@ extractObject = ExtractLines(original_img_obj, text, 3, 30, font_path)
 
 
 def test_line_extraction(utils):
+    """Test the line extraction process by comparing actual extracted lines with expected results.
+
+    Args:
+        utils: A utility object that contains helper functions for testing,
+               specifically an `is_same_img` function that compares two images.
+    """
     line_images = extractObject.extract_lines()
     expected_extracted_lines_dir = Path(
         "./tests/line_extraction/data/expected_line_extraction"
@@ -35,3 +42,57 @@ def test_line_extraction(utils):
             expected_line_image = Image.open(expected_extracted_lines_path)
             actual_line_image = Image.open(actual_line_images_path)
             assert utils.is_same_img(expected_line_image, actual_line_image)
+
+
+def test_blank_image(utils):
+    """Test the creation of a blank image.
+
+    Args:
+        utils: A utility object that contains helper functions for testing,
+               specifically an `is_same_img` function that compares two images.
+    """
+    blank_img = extractObject.get_blank_img()
+    expected_blank_image_path = Path(
+        "./tests/line_extraction/data/expected_blank_image/expected_blank_image.png"
+    )
+    with tempfile.TemporaryDirectory() as tempdirname:
+        actual_blank_image_path = Path(tempdirname) / "actual_blank_image.png"
+        blank_img.save(actual_blank_image_path)
+        expected_blank_image = Image.open(expected_blank_image_path)
+        actual_blank_image = Image.open(actual_blank_image_path)
+        assert utils.is_same_img(expected_blank_image, actual_blank_image)
+
+
+def test_max_width():
+    """Test the calculation of the maximum width of the lines."""
+    blank_img = extractObject.get_blank_img()
+    font = ImageFont.truetype(font_path, 30)
+    lines = text.split("\n")
+    max_width, line_bboxes = extractObject.get_max_width(lines, blank_img, font)
+    assert max_width == 1169
+
+
+def test_line_images(utils):
+    """Test the creation of line images.
+
+    Args:
+        utils: A utility object that contains helper functions for testing,
+               specifically an `is_same_img` function that compares two images.
+    """
+    blank_img = extractObject.get_blank_img()
+    font = ImageFont.truetype(font_path, 30)
+    lines = text.split("\n")
+    max_width, line_bboxes = extractObject.get_max_width(lines, blank_img, font)
+    line_images = extractObject.get_line_images(max_width, line_bboxes)
+    expected_line_images_dir = Path("./tests/line_extraction/data/expected_line_images")
+    with tempfile.TemporaryDirectory() as tempdirname:
+
+        for idx, line_image in enumerate(line_images):
+            actual_line_images_path = Path(tempdirname) / f"actual_line_image{idx}.png"
+            line_image.save(actual_line_images_path)
+            expected_line_images_path = (
+                expected_line_images_dir / f"expected_line_image{idx}.png"
+            )
+            expected_line_image = Image.open(expected_line_images_path)
+            actual_line_image = Image.open(actual_line_images_path)
+            assert utils.is_same_img(actual_line_image, expected_line_image)
